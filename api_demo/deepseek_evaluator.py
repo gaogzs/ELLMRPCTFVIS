10affdcc5daf0ec9a8ef50e27b0799a1636f8b02
Xiwei Gao <gaogzs@163.com>
2025-03-12 01:15:37 +0000

    Update 1 shot prompting and evaluation

--- api_demo/deepseek_evaluator.py
+++ api_demo/deepseek_evaluator.py
@@ -33,10 +33,13 @@ def get_response(messages, temperature=1.3):
     print(response.choices[0].message.content)
     return response.choices[0].message
 
-def llm_evaluation(messages, eval_type="single-scoring", model="deepseek-chat"):
+def llm_evaluation(messages, eval_type="single-scoring", model="deepseek-chat", shots=0):
     if eval_type == "single-scoring":
-        temperature=0.1
-        eval_prompt = eval_prompts[eval_type].copy()
+        temperature = 0.1
+        if shots is not None:
+            eval_prompt = eval_prompts[eval_type][:shots * 2 + 1].copy()
+        else:
+            eval_prompt = eval_prompts[eval_type].copy()
         
         user_input = {"role": "user", "content": str(messages)}
         eval_prompt.append(user_input)
@@ -60,8 +63,11 @@ def llm_evaluation(messages, eval_type="single-scoring", model="deepseek-chat"):
             
         return (eval_score, eval_reason)
     elif eval_type == "multiple-scoring-amazon-bedrock":
-        temperature=0.1
-        eval_prompt = eval_prompts[eval_type].copy()
+        temperature = 0.1
+        if shots is not None:
+            eval_prompt = eval_prompts[eval_type][:shots * 2 + 1].copy()
+        else:
+            eval_prompt = eval_prompts[eval_type].copy()
         
         user_input = {"role": "user", "content": str(messages)}
         eval_prompt.append(user_input)
@@ -75,6 +81,11 @@ def llm_evaluation(messages, eval_type="single-scoring", model="deepseek-chat"):
         
         response_content = str(response.choices[0].message.content)
         eval_score = json.loads(response_content)
+        average_score = 0
+        for key in eval_score.keys():
+            if key != "average":
+                average_score += eval_score[key]
+        eval_score["average"] = round(average_score / len(eval_score.keys()), 3)
         
         print(eval_score)
         
@@ -88,7 +99,7 @@ def llm_evaluation(messages, eval_type="single-scoring", model="deepseek-chat"):
         print(f"Evaluation type {eval_type} not recognised!")
         return None
 
-def test_slicing(sample_messages, eval_type, model):
+def test_slicing(sample_messages, eval_type, model, shots):
     
     histories = []
     
@@ -99,10 +110,10 @@ def test_slicing(sample_messages, eval_type, model):
         new_entry["messages"] = sliced_messages
         new_entry["type"] = eval_type
         
-        evaluation = llm_evaluation(sliced_messages, eval_type=eval_type, model="deepseek-chat")[0]
+        evaluation = llm_evaluation(sliced_messages, eval_type=eval_type, model="deepseek-chat", shots=shots)[0]
         new_entry["chat_evaluation"] = evaluation
         
-        evaluation, reason = llm_evaluation(sliced_messages, eval_type=eval_type, model="deepseek-reasoner")
+        evaluation, reason = llm_evaluation(sliced_messages, eval_type=eval_type, model="deepseek-reasoner", shots=shots)
         new_entry["reasoner_evaluation"] = evaluation
         new_entry["reason_reasoning"] = reason
         
@@ -117,7 +128,6 @@ if __name__ == "__main__":
     
     replace_prompt = "SpeakerHGG-X"
     test_histories = []
-    i = 0
     
     for test_sample in samples:
         sample_messages = test_sample["messages"]
@@ -127,7 +137,7 @@ if __name__ == "__main__":
                     message["content"] = replace_prompt
                     break
         
-        test_history = test_slicing(sample_messages, eval_type=eval_type, model=eval_model)
+        test_history = test_slicing(sample_messages, eval_type=eval_type, model=eval_model, shots=1)
         test_histories.append(test_history)
         
         with open(output_dir, "w") as f:
---
+++ api_demo/output_evaluation.py
@@ -0,0 +1,71 @@
+import os
+import json
+from matplotlib import pyplot as plt
+
+cur_dir = os.path.dirname(__file__)
+
+visual_dir_rel = "visualisations"
+visual_dir = os.path.join(cur_dir, visual_dir_rel)
+
+def single_value_scoring_by_prompts(dir, score_keyword, store_keyword=""):
+    filename = dir.split(".")[0]
+    output_log = []
+    prompts_dir = os.path.join(cur_dir, dir)
+    eval_hists = []
+    with open(prompts_dir, "r") as f:
+        eval_hists = json.load(f)
+    
+    # Iterate through the evaluation history
+    score_hists = []
+    i = 0
+    for eval_hist in eval_hists:
+        score_hists.append([])
+        for eval_item in eval_hist:
+            score_hists[i].append(eval_item[score_keyword])
+        i += 1
+    
+    # Plot the scores in a line graph
+    for prompt_index, scores in zip(range(len(score_hists)), score_hists):
+        plt.plot(scores, label=prompt_index)
+    plt.legend()
+    
+    if store_keyword == "":
+        plt.savefig(os.path.join(visual_dir, f"{filename}_{score_keyword}_line_graph.png"))
+    else:
+        plt.savefig(os.path.join(visual_dir, f"{filename}_{store_keyword}_{score_keyword}_line_graph.png"))
+
+def multi_value_scoring(dir, score_keyword, metrics_used=["average"], store_keyword=""):
+    filename = dir.split(".")[0]
+    output_log = []
+    prompts_dir = os.path.join(cur_dir, dir)
+    eval_hists = []
+    with open(prompts_dir, "r") as f:
+        eval_hists = json.load(f)
+    
+    if metrics_used is None:
+        metrics_used = list(eval_hists[0][0][score_keyword].keys())
+    # Iterate through the evaluation history
+    score_hists = {}
+    for metric in metrics_used:
+        score_hists[metric] = []
+    for eval_hist in eval_hists:
+        for eval_item in eval_hist:
+            for metric in metrics_used:
+                score_hists[metric].append(eval_item[score_keyword][metric])
+
+    # Plot the scores in a line graph
+    for metric in metrics_used:
+        if metric == "average":
+            plt.plot(score_hists[metric], label=metric, linewidth=3)
+        else:
+            plt.plot(score_hists[metric], label=metric, linewidth=1)
+    plt.legend(fontsize='small')
+    
+    if store_keyword == "":
+        plt.savefig(os.path.join(visual_dir, f"{filename}_{score_keyword}_line_graph.png"))
+    else:
+        plt.savefig(os.path.join(visual_dir, f"{filename}_{store_keyword}_{score_keyword}_line_graph.png"))
+    
+
+if __name__ == "__main__":
+    multi_value_scoring(dir="output_multiple-scoring-amazon-bedrock_1_shot.json", score_keyword="chat_evaluation", metrics_used=None)
