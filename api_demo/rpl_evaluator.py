30ca23f0b58637ed41ed499074b0d7d67ce8e11b
Xiwei Gao <gaogzs@163.com>
2025-04-22 20:31:33 +0100

    Made a basic code for FO log operation evaluation

--- api_demo/rpl_evaluator.py
+++ api_demo/rpl_evaluator.py
@@ -3,53 +3,37 @@ import json
 from openai import OpenAI
 import re
 import random
+from z3 import *
 
-import fact_graph
-from fact_graph import FactGraph
 from chatbot import ChatBotDummy
 
 do_it_command = "move on to function calls"
 
 eval_prompts = {
-    "graph_maker": [
+    "formula_maker": [
         {
             "role": "system",
             "content": f"""
-You are a helpful AI assistant that creates a graph of given format based on a given role-playing scenario happened between a user and an AI.
-The input will be given in the format \"**Current Graph**[graph_content] **Historical Conversation**[conversation] **Latest Conversation**[conversation]\". Historical Conversation represents the history of the role-play scenario that was used to make the Current Graph, it can be empty. Your task is to edit the Current Graph based on the content of how content of Latest Conversation impact Historical Conversation. You can choose to add new nodes, add new edges, remove nodes, remove edges, change any attribute of an existing node including the name, and change the content of existing edges. You should not change the content of the Historical Conversation, but you can add new nodes or edges to the Current Graph based on the Latest Conversation.
-Example: If the Historical Conversation includes \"John is living in London\" and Current Graph includes an edge \"John ->([source_node] live in [target_node]) London\" and the Latest Conversation includes \"John moved to Paris\", then you should remove the edge \"John -> London ([source_node] live in [target_node])\" and add the edge \"John -> Paris ([source_node] live in [target_node])\".
-Your output should be in two steps: When the user first sends the input, you should reply with pure text with a brief reasoning about what you are planning to do and why, then after the user says \"{do_it_command}\", you will conduct all your desired edit to the graph using the provided functions.
-The graph you make will be a general summary of the role-playing scenario, like a mind map or a reading note. It should not be too detailed, but should include all the important information.
-***Graph Explained***
-{fact_graph.desc_fact_graph}
-{fact_graph.desc_fact_node}
-{fact_graph.desc_node_types}
-{fact_graph.desc_info_types}
-{fact_graph.desc_edge_content}
-"""
-        }
-    ],
-    "question_asker": [
-        {
-            "role": "system",
-            "content": f"""
-You are a helpful assistant specialised at analysing a role-playing scenario happened between a user and an AI. Later you will be presented with a convertsation history and being asked a question.
-The input will be in the format:\"**Content**[content]**Question**[question]\", where Content is the role-playing scenario and Question is the yes/no question you need to answer.
-In the question there may exist special objects in the token format of  \"[name](info_type=[info_type])\", these special objects will be the point of interest in your evaluation, and you need to evaluate them in the criteria of their repective info_type.
-{fact_graph.desc_info_types}
-Your output should be in the format:
-\"**Reasoning**
+You are a helpful AI assistant that creates a first-order logical formula based on a given role-playing scenario happened between a user and an AI in order to maintain its logical consistency.
+The input will be given in the format \"AI:[content] User:[content] AI:[content] ...\"
+Your output will be in the format:
+\"
+**Reasoning**
 [reasoning]
-**Answer**
-[answer]\", where you should first give a brief step-by-step reasoning in the [reasoning] about how you come to the answer, and then give the final answer in [answer]. The [reasoning] part should be concise and generally no more than 100 words, and the [answer] should be either \"True\", \"False\" or "Unkown". In some cases where the content is not implicitly irrelevent with the question, but you think it should be(Example:"John is ice-skating" and "Is John currently in a grassland?"), still answer in only \"True\" or \"False\", otherwise(Example:"John is ice-skating", "Is the apple red?") answer \"Unkown\".
+**SAT definition**
+[formula]
+\"
+Where [reasoning] is your explanation and chain of thought about what you are planning to do, and [formula] is the first-order logical formula definition you created based on the input and your previous thoughts. The definition should be given in plain text of SMT-LIB format that can be parsed directly to a Z3 solver. So there should not be any beginning and ending \"```\" and \"smtlib\" notion. All comments should follow a prefix \";;\"
+**Guidelines of Creating Formula**
+Most of objects that appeared in the story should be defined as contants that has type of \"Object\". The type of \"Object\" is defined as a set of all objects that appeared in the story. And in order to state further facts about story, you can define functions as the relation between declared objects. No other sort need to be declared except for Object.
+Be note tha relation function can only exist between already-declared constants!
+Functions can come in serveral basic categories:
+- Simple relation: \"foo (Object, Object) Bool\", which simply states the existence of a relation, the input field can be more than 2. Example: \"is_friend (Alice, Bob)\" means Alice and Bob are friends.
+- Exclusive relation: \"foo (Object, Object) Bool\", which states that the unidirectional relation from the first object A to the second object B is exclusive, and cannot happen between A and C or any other object. Example: \"is_race (Alice, Human)\" means Alice is a human, and Alice cannot be any other type of object, in this case \"is_race (Alice, Human) and is_race (Alice, Dog)\" should be false, since Alice cannot be human and dog at the same time. In this case the definition should also include something like \"(assert (implies (is_race a b) (forall ((x Object)) (implies (not (= x b)) (not (is_race a x))))))\" as a part of CNF.
+- Time contrained exclusive relation: \"foo (Object, Object, Int) Bool\", which states that the unidirectional relation from the first object A to the second object B is exclusive in a certain time period. Example: \"locates_in (Alice, London, T)\" means Alice is in London at time T (integer type) and Alice cannot be in any other place at the same time, but can be in other places if the time is different. so \"locates_in (Alice, London, T) and \"locates_in (Alice, Paris, T)\" is false since Alice cannot be in two different cities at the same time, but \"locates_in (Alice, London, T) and \"locates_in (Alice, Paris, T+1)\" is acceptable, since it means that Alice has moved from London to Paris at since time T. And extra formula should be added to the root CNF to make such logic valid, like \"(assert (implies (locates_in a b c) (forall ((x Object)) (implies (not (= x b)) (not (locates_in a x c))))))\". Time expression can be just an abstruct representation like Tminus, T0, T1, Tplus -- where Tminus means past, T0 means present at beginning of the story section, T1 means the new present at the end of story section and Tplus means future. All of these time expressions should be declared as a constant of type Int.
 """
         }
     ]
-}
-
-input_templates = {
-    "graph_maker": "**Current Graph**\n[current_graph]\n**Historical Conversation**\n[hist_conversation]\n**Latest Conversation**\n[latest_conversation]",
-    "question_asker": "**Content**\n[conversation]\n**Question**\n[question]"
 }
 
 def is_in_openai_form(message: str) -> bool:
@@ -78,107 +62,68 @@ def openai_form_to_str(message: str) -> str:
             message_str += f"{role}: {message['content']}\n"
     return message_str
 
-def ask_question(conversations: str, fact_graph: FactGraph, client: OpenAI, model: str, question_num: int=-1, question_type: int=-1):
-    if fact_graph.graph_empty():
-        return []
-    if question_type == -1:
-        question_type = 1
-    question_texts = []
-    if question_type == 0:
-        sampled_nodes = fact_graph.sample_nodes(question_num)
-        for node_name, node_data in sampled_nodes:
-            question_text = f"Does the role-playing content respect the fact that object {node_name}(info_type={node_data["info_type"]}) exist as a {node_data["node_type"]} in the story as a background?"
-            question_texts.append(question_text)
-    elif question_type == 1:
-        sampled_edges = fact_graph.sample_edges(question_num)
-        for source_node, target_node, edge_data in sampled_edges:
-            source_data = fact_graph.get_node_data(source_node)
-            target_data = fact_graph.get_node_data(target_node)
-            content = edge_data["content"]
-            content = content.replace("[source_node]", f"{source_node}(info_type={source_data["info_type"]})")
-            content = content.replace("[target_node]", f"{target_node}(info_type={target_data["info_type"]})")
-            question_text = f"Does the role-playing content respect the fact \"{content}\" in the story as a background?"
-            question_texts.append(question_text)
-    else:
-        raise ValueError("question_type not recognized")
+def check_brackets(smtlib_str):
+    open_count = smtlib_str.count('(')
+    close_count = smtlib_str.count(')')
+    difference = open_count - close_count
+    if difference > 0:
+        print(f"[!] Mismatch: {open_count} '(' vs {close_count} ')'. Adding {difference} closing ')'s.")
+        return smtlib_str + (')' * (difference))
+    return smtlib_str
     
-    input_message = input_templates["question_asker"]
-    input_message = input_message.replace("[conversation]", conversations)
-    
-    logs = []
-    for question_text in question_texts:
-        input_message_i = input_message.replace("[question]", question_text)
-        user_message = {"role": "user", "content": input_message_i}
-        response = client.chat.completions.create(messages=eval_prompts["question_asker"] + [user_message], model=model, temperature=0)
-        response_message = response.choices[0].message.content
-        logs.append({"question": question_text, "response": response_message})
-
-    return logs
-    
-        
 class RPEvaluationSession():
     def __init__(self, client: OpenAI, model: str, history: list = None) -> None:
         self.rp_history = history if history is not None else ""
         self.client = client
         self.model = model
-        self.fact_graph = FactGraph()
+        self.formulas = []
         self.logs = []
 
     def append_conversation(self, lastest_conversation: str) -> None:
         
-        complete_messages = eval_prompts["graph_maker"].copy()
+        complete_messages = eval_prompts["formula_maker"].copy()
         
         if is_in_openai_form(lastest_conversation):
             lastest_conversation = openai_form_to_str(lastest_conversation)
-        beginning_graph = self.fact_graph.to_json()
-        message = input_templates["graph_maker"]
-        message = message.replace("[current_graph]", self.fact_graph.to_str())
-        message = message.replace("[hist_conversation]", self.rp_history)
-        message = message.replace("[latest_conversation]", lastest_conversation)
+        message = lastest_conversation
         
         user_message = {"role": "user", "content": message}
         complete_messages.append(user_message)
         
-        response = self.client.chat.completions.create(messages=complete_messages, model=self.model, temperature=0.7)
-        reasoning_response = response.choices[0].message
-        complete_messages.append(reasoning_response)
+        response = self.client.chat.completions.create(messages=complete_messages, model=self.model, temperature=0)
+        complete_response = response.choices[0].message
+        complete_messages.append(complete_response)
         
-        reasoning_text = reasoning_response.content
+        reasoning_text, formula_text = complete_response.content.split("**SAT definition**")
+        reasoning_text = reasoning_text.split("**Reasoning**")[1].strip()
         print(reasoning_text)
-        user_message = {"role": "user", "content": do_it_command}
-        complete_messages.append(user_message)
-        response = self.client.chat.completions.create(messages=complete_messages, model=self.model, tools=fact_graph.tool_api, temperature=0)
-        tool_call_response = response.choices[0].message
-        complete_messages.append(tool_call_response)
-        
-        qa_logs = self.ask_question(lastest_conversation)
-
-        function_call_logs = []
-        for tool_call in tool_call_response.tool_calls:
-            function_name = tool_call.function.name
-            arguments = json.loads(tool_call.function.arguments)
-            self.fact_graph.exec_function(function_name, arguments)
-            
-            function_call_logs.append({
-                "function_name": function_name,
-                "arguments": arguments
-            })
+        print(formula_text)
         
         self.rp_history += lastest_conversation + "\n"
         
+        formula_text = check_brackets(formula_text)
+        current_formula = parse_smt2_string(formula_text)
+        result = None
+        
+        if self.formulas:
+            last_formula = self.formulas[-1]
+            satisfiable = Implies(And(*last_formula), And(*current_formula))
+            
+            solver = Solver()
+            solver.add(satisfiable)
+            result = str(solver.check())
+            
+        self.formulas.append(parse_smt2_string(formula_text.replace("T0", "Tminus").replace("T1", "T0")))
+        
+        pretty_formula = current_formula.sexpr()
         
         new_log = {
-            "beginning_graph": beginning_graph,
             "conversation": lastest_conversation,
-            "qa_logs": qa_logs,
             "reasoning": reasoning_text,
-            "function_calls": function_call_logs,
-            "ending_graph": self.fact_graph.to_json()
+            "formula": pretty_formula,
+            "result": result,
         }
         self.logs.append(new_log)
-    
-    def ask_question(self, conversation: str, question_num: int=-1, question_type: int=-1) -> None:
-       return ask_question(conversation, self.fact_graph, self.client, self.model, question_num, question_type)
         
     
     def export_logs(self, file_path: str) -> None:
