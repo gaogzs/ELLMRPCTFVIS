447ba9d9360a6515b7e91eab75e394b6e13e8fb3
Xiwei Gao <gaogzs@163.com>
2025-05-14 18:20:23 +0100

    Moved promts to independant txt files

--- api_demo/rpl_evaluator.py
+++ api_demo/rpl_evaluator.py
@@ -7,8 +7,8 @@ from z3 import *
 import z3.z3util
 
 from chatbot import ChatBotDummy
-from prompts_definition import *
 from str_to_z3_parser import Z3Builder, parse_z3
+from prompt_loader import PromptLoader
 
 instruction_templates = {
     "timeline_maker": """
@@ -91,14 +91,32 @@ class RPEvaluationSession():
         self.logs = []
         self.z3_builder = Z3Builder(self.get_z3_function)
         
+        self.prompt_loader = PromptLoader("prompts/")
+        
     
-    def get_declarations_str(self) -> str:
+    def get_all_declarations_str(self) -> str:
         out_str = ""
         for name, meaning in self.objects.items():
             out_str += f"{name}: {meaning}\n"
         for name, info in self.relations.items():
             out_str += f"{name}({", ".join(info["params"])}): {info["meaning"]}\n"
         return out_str
+    
+    def get_keyed_declarations_str(self, obj_keys: list, rel_keys: list) -> str:
+        obj_str = ""
+        for key in obj_keys:
+            if key in self.objects:
+                obj_str += f"{key}: {self.objects[key]}\n"
+            else:
+                print(f"Warning: {key} not found in declarations.")
+        rel_str = ""
+        for key in rel_keys:
+            if key in self.relations:
+                info = self.relations[key]
+                rel_str += f"{key}({", ".join(info["params"])}): {info["meaning"]}\n"
+            else:
+                print(f"Warning: {key} not found in declarations.")
+        return obj_str, rel_str
     
     def get_timeline_str(self) -> str:
         out_str = ""
@@ -112,112 +130,13 @@ class RPEvaluationSession():
         else:
             raise ValueError(f"Relation {name} not found in declarations.")
     
-    def handle_timeline_maker(self, lastest_conversation: str) -> str:
-        
-        sys_prompt = [{"role": "system", "content": sys_prompts["timeline_maker"]}]
-        if self.rp_history:
-            sys_prompt.append({"role": "user", "content": "\n".join(self.rp_history)})
-            sys_prompt.append({"role": "assistant", "content": "-- **Reasoning**\n[Hidden]\n-- **Timeline Definitions**\n" + self.get_timeline_str()})
-        bot = ChatBotDummy(self.client, self.model, sys_prompt)
-        
-        message = instruction_templates["timeline_maker"].format(story=lastest_conversation)
-        
-        complete_response = bot.send_message(message, record=False, temperature=0.2)
-        print("Timeline Maker Response:")
-        print(complete_response)
-        
-        reasoning_text, timeline_text = divide_response_parts(complete_response)
-        
-        # Parse the timeline
-        for declare_line in timeline_text.splitlines():
-            if ":" in declare_line:
-                time_point_name, time_point_meaning = declare_line.split(":", 1)
-                time_point_name = time_point_name.strip()
-                time_point_meaning = time_point_meaning.strip()
-                if time_point_name in self.timeline:
-                    print(f"Warning: {time_point_name} already exists in timeline.")
-                self.timeline[time_point_name] = time_point_meaning
-        
-        return timeline_text
-    
-    def handle_declaration_maker(self, lastest_conversation: str) -> tuple[list, list]:
-        
-        sys_prompt = [{"role": "system", "content": sys_prompts["declaration_maker"]}]
-        bot = ChatBotDummy(self.client, self.model, sys_prompt)
-        
-        declarations_str = self.get_declarations_str()
-        message = instruction_templates["declaration_maker"].format(story=lastest_conversation, reference=declarations_str)
-        
-        complete_response = bot.send_message(message, record=False, temperature=0.2)
-        print("Declaration Maker Response:")
-        print(complete_response)
-        
-        objects_text, relations_text, replenishment_text = divide_response_parts(complete_response)
-        
-        objects_text += "\n" + replenishment_text
-        
-        # Parse the object declarations
-        obj_keys = []
-        for declare_line in objects_text.splitlines():
-            if ":" in declare_line:
-                obj_name, obj_meaning = declare_line.split(":", 1)
-                obj_name = obj_name.strip()
-                obj_meaning = obj_meaning.strip()
-                if obj_name in self.objects:
-                    print(f"Warning: {obj_name} already exists in declarations.")
-                self.objects[obj_name] = obj_meaning
-                obj_keys.append(obj_name)
-        
-        # Parse the relation declarations
-        rel_keys = []
-        for declare_line in relations_text.splitlines():
-            if ":" in declare_line:
-                rel_name, rel_meaning = declare_line.split(":")[:2]
-                rel_name = rel_name.strip()
-                rel_meaning = rel_meaning.strip()
-                rel_just_name = rel_name.split("(")[0]
-                rel_params = get_relation_params(rel_meaning)
-                rel_z3_func = Function(rel_just_name, *[IntSort() for param in rel_params], BoolSort())
-                if rel_just_name in self.relations:
-                    print(f"Warning: {rel_just_name} already exists in declarations.")
-                self.relations[rel_just_name] = {"params": rel_params, "meaning": rel_meaning, "function": rel_z3_func}
-                
-                
-                rel_keys.append(rel_name)
-        
-        return obj_keys, rel_keys
-
-    def handle_semantic_definer(self, lastest_conversation: str, obj_keys: list, rel_keys: list) -> tuple[list, list, str, str]:
-        
-        sys_prompt = [{"role": "system", "content": sys_prompts["semantic_definer"]}]
-        bot = ChatBotDummy(self.client, self.model, sys_prompt)
-        
-        current_declarations = ""
-        for key in obj_keys:
-            if key in self.objects:
-                current_declarations += f"{key}: {self.objects[key]}\n"
-            else:
-                print(f"Warning: {key} not found in declarations.")
-        for key in rel_keys:
-            if key in self.relations:
-                current_declarations += f"{key}: {self.relations[key]}\n"
-            else:
-                print(f"Warning: {key} not found in declarations.")
-                
-        message = instruction_templates["semantic_definer"].format(declarations=current_declarations)
-        
-        complete_response = bot.send_message(message, record=False, temperature=0.1)
-        print("Semantic Definer Response:")
-        print(complete_response)
-        
-        reasoning_text, definitions_text = divide_response_parts(complete_response)
-        
-        # Incase there are some re-defined time-sensitive relations, we need to add them to the declarations
-        explicit_formulas = []
-        for declare_line in definitions_text.splitlines():
-            if "[exclusive_arg]" in declare_line:
-                rel_just_name = declare_line.split("(")[0]
-                edited_params = get_relation_params(declare_line)
+    def parse_internal_formula(self, definitions_text: str) -> list:
+        formulas = []
+        new_definition_text = ""
+        for definition_line in definitions_text.splitlines():
+            if "[exclusive_arg]" in definition_line:
+                rel_just_name = definition_line.split("(")[0]
+                edited_params = get_relation_params(definition_line)
                 original_params = self.relations[rel_just_name]["params"]
                 rel_z3_func = self.relations[rel_just_name]["function"]
                 
@@ -243,35 +162,118 @@ class RPEvaluationSession():
                 rhs_func = rel_z3_func(*[Int(param) for param in ref_r])
                 constraint_expr = constrain_pairs[0] if len(constrain_pairs) == 1 else And(*constrain_pairs)
                 explicit_formula = ForAll([Int(param) for param in scope_params], Implies(And(lhs_func, rhs_func), constraint_expr))
-                explicit_formulas.append(explicit_formula)
+                formulas.append(explicit_formula)
             else:
-                new_definition_text += declare_line + "\n"
+                new_definition_text += definition_line + "\n"
+        return formulas, new_definition_text
+    
+    def handle_timeline_maker(self, lastest_conversation: str) -> str:
+        
+        sys_prompt = [{"role": "system", "content": self.prompt_loader.load_sys_prompts("timeline_maker")}]
+        if self.rp_history:
+            sys_prompt.append({"role": "user", "content": "\n".join(self.rp_history)})
+            sys_prompt.append({"role": "assistant", "content": "-- **Reasoning**\n[Hidden]\n-- **Timeline Definitions**\n" + self.get_timeline_str()})
+        bot = ChatBotDummy(self.client, self.model, sys_prompt)
+        
+        message = instruction_templates["timeline_maker"].format(story=lastest_conversation)
+        
+        complete_response = bot.send_message(message, record=False, temperature=0.2)
+        print("Timeline Maker Response:")
+        print(complete_response)
+        
+        reasoning_text, timeline_text = divide_response_parts(complete_response)
+        
+        # Parse the timeline
+        for definition_line in timeline_text.splitlines():
+            if ":" in definition_line:
+                time_point_name, time_point_meaning = definition_line.split(":", 1)
+                time_point_name = time_point_name.strip()
+                time_point_meaning = time_point_meaning.strip()
+                if time_point_name in self.timeline:
+                    print(f"Warning: {time_point_name} already exists in timeline.")
+                self.timeline[time_point_name] = time_point_meaning
+        
+        return timeline_text
+    
+    def handle_declaration_maker(self, lastest_conversation: str) -> tuple[list, list]:
+        
+        sys_prompt = [{"role": "system", "content": self.prompt_loader.load_sys_prompts("declaration_maker")}]
+        bot = ChatBotDummy(self.client, self.model, sys_prompt)
+        
+        declarations_str = self.get_all_declarations_str()
+        message = instruction_templates["declaration_maker"].format(story=lastest_conversation, reference=declarations_str)
+        
+        complete_response = bot.send_message(message, record=False, temperature=0.2)
+        print("Declaration Maker Response:")
+        print(complete_response)
+        
+        objects_text, relations_text, replenishment_text = divide_response_parts(complete_response)
+        
+        objects_text += "\n" + replenishment_text
+        
+        # Parse the object declarations
+        obj_keys = []
+        for definition_line in objects_text.splitlines():
+            if ":" in definition_line:
+                obj_name, obj_meaning = definition_line.split(":", 1)
+                obj_name = obj_name.strip()
+                obj_meaning = obj_meaning.strip()
+                if obj_name in self.objects:
+                    print(f"Warning: {obj_name} already exists in declarations.")
+                self.objects[obj_name] = obj_meaning
+                obj_keys.append(obj_name)
+        
+        # Parse the relation declarations
+        rel_keys = []
+        for definition_line in relations_text.splitlines():
+            if ":" in definition_line:
+                rel_name, rel_meaning = definition_line.split(":")[:2]
+                rel_name = rel_name.strip()
+                rel_meaning = rel_meaning.strip()
+                rel_just_name = rel_name.split("(")[0]
+                rel_params = get_relation_params(rel_name)
+                rel_z3_func = Function(rel_just_name, *[IntSort() for param in rel_params], BoolSort())
+                if rel_just_name in self.relations:
+                    print(f"Warning: {rel_just_name} already exists in declarations.")
+                self.relations[rel_just_name] = {"params": rel_params, "meaning": rel_meaning, "function": rel_z3_func}
+                
+                
+                rel_keys.append(rel_just_name)
+        
+        return obj_keys, rel_keys
+
+    def handle_semantic_definer(self, lastest_conversation: str, obj_keys: list, rel_keys: list) -> tuple[list, list, str, str]:
+        
+        sys_prompt = [{"role": "system", "content": self.prompt_loader.load_sys_prompts("semantic_definer")}]
+        bot = ChatBotDummy(self.client, self.model, sys_prompt)
+        
+        obj_str, rel_str = self.get_keyed_declarations_str(obj_keys, rel_keys)
+        current_declarations = obj_str + "\n" + rel_str
+                
+        message = instruction_templates["semantic_definer"].format(declarations=current_declarations)
+        
+        complete_response = bot.send_message(message, record=False, temperature=0.1)
+        print("Semantic Definer Response:")
+        print(complete_response)
+        
+        reasoning_text, definitions_text = divide_response_parts(complete_response)
+        
+        # Incase there are some re-defined time-sensitive relations, we need to add them to the declarations
+        explicit_formulas, new_definition_text = self.parse_internal_formula(definitions_text)
         
         return obj_keys, rel_keys, new_definition_text, explicit_formulas
     
     def handle_formula_maker(self, lastest_conversation: str, obj_keys: list, rel_keys: list, predefined_text: str) -> AstVector:
         
-        sys_prompt = [{"role": "system", "content": sys_prompts["formula_maker"]}]
+        sys_prompt = [{"role": "system", "content": self.prompt_loader.load_sys_prompts("formula_maker")}]
         bot = ChatBotDummy(self.client, self.model, sys_prompt)
         
         # Make up the prompt from data
-        objects_str = ""
-        for key in obj_keys:
-            if key in self.declarations:
-                objects_str += f"{key}: {self.declarations[key]}\n"
-            else:
-                print(f"Warning: {key} not found in declarations.")
-
-        relations_str = ""
-        for key in rel_keys:
-            if key in self.declarations:
-                relations_str += f"{key}: {self.declarations[key]}\n"
-            else:
-                print(f"Warning: {key} not found in declarations.")
+        obj_str, rel_str = self.get_keyed_declarations_str(obj_keys, rel_keys)
         
         timeline_str = self.get_timeline_str()
         
-        message = instruction_templates["formula_maker"].format(story=lastest_conversation, objects=objects_str, relations=relations_str, predefined_properties=predefined_text, existing_timelines=timeline_str)
+        message = instruction_templates["formula_maker"].format(story=lastest_conversation, objects=obj_str, relations=rel_str, predefined_properties=predefined_text, existing_timelines=timeline_str)
         
         
         complete_response = bot.send_message(message, record=True, temperature=0)
@@ -281,9 +283,10 @@ class RPEvaluationSession():
         reasoning_text, plan_text, formula_text = divide_response_parts(complete_response)
 
         current_formula = []
-        for formula in formula_text.splitlines():
-            parsed_formula = parse_z3(self.z3_builder, formula)
-            current_formula.append(parsed_formula)
+        if formula_text != "None":
+            for formula in formula_text.splitlines():
+                parsed_formula = parse_z3(self.z3_builder, formula)
+                current_formula.append(parsed_formula)
         
         return current_formula
         
@@ -297,19 +300,19 @@ class RPEvaluationSession():
         self.rp_history.append(lastest_conversation)
         
         complete_current_formula = semantic_defined_formulas + current_formula
-        combined_past_formula = sum(self.formulas, [])
-        combined_past_formula += semantic_defined_formulas
-        
-        result = self.solve_combined_formulas(combined_past_formula)
+        result = None
+        if self.formulas:
+            combined_past_formula = sum(self.formulas, [])
+            combined_past_formula += semantic_defined_formulas
+            
+            result = self.solve_combined_formulas(combined_past_formula)
             
         self.formulas.append(complete_current_formula)
         
         pretty_formula = "\n".join([str(formula) for formula in complete_current_formula])
         
-        for name, meaning in self.objects.items():
-            current_declarations += f"{name}: {meaning}\n"
-        for name, info in self.relations.items():
-            current_declarations += f"{name}({", ".join(info["params"])}): {info["meaning"]}\n"
+        obj_str, rel_str = self.get_keyed_declarations_str(obj_keys, rel_keys)
+        current_declarations = obj_str + "\n" + rel_str
         
         new_log = {
             "conversation": lastest_conversation,
@@ -333,6 +336,9 @@ class RPEvaluationSession():
         # Check if the formulas are satisfiable
         result = solver.check()
         
+        print(solver.assertions())
+        print("Solver result:", result)
+        
         if result == sat:
             return 0
         elif result == unsat:
@@ -345,7 +351,7 @@ class RPEvaluationSession():
         new_log = {
             "full_conversation": "\n".join(self.rp_history),
             "full_formulas": "\n".join([str(formula) for formula in self.formulas]),
-            "full_declarations": self.get_declarations_str(),
+            "full_declarations": self.get_all_declarations_str(),
             "full_timeline": self.get_timeline_str(),
         }
         with open(file_path, "w", encoding="utf-8") as f:
